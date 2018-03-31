a1427e11ab49953246d4d395f52a9e1d8453dec8
David Doan <s3686810@student.rmit.edu.au>
2018-03-31 17:58:08 +1100

    Added getters and setters to Staff class

--- src/Staff.java
+++ src/Staff.java
@@ -9,4 +9,64 @@ public abstract class Staff
     private String phoneNumber;
     private String ID;
 
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setUserName(String uName)
+    {
+        this.userName = uName;
+    }
+
+    public String getUserName()
+    {
+        return this.userName;
+    }
+
+    public void setPassword(String pWord)
+    {
+        this.password = pWord;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public void setEmailAddress(String email)
+    {
+        this.emailAddress = email;
+    }
+
+    public String getEmailAddress()
+    {
+        return this.emailAddress;
+    }
+
+    public void setPhoneNumber(String phone)
+    {
+        this.phoneNumber = phone;
+    }
+
+    public String getPhoneNumber()
+    {
+        return this.phoneNumber;
+    }
+
+    public void setID(String IDNum)
+    {
+        this.ID = IDNum;
+    }
+
+    public String getID()
+    {
+        return this.ID;
+    }
 }
