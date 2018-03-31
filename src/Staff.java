daaee9de61736a6ce27eb3b041a20823e86ccb25
David Doan <s3686810@student.rmit.edu.au>
2018-03-31 17:47:43 +1100

    Created abstract class that all actors will extend from

---
+++ src/Staff.java
@@ -0,0 +1,12 @@
+public abstract class Staff
+// This will be the abstract class that all of the actors will extend from
+{
+    public abstract void login();
+    private String name;
+    private String userName;
+    private String password;
+    private String emailAddress;
+    private String phoneNumber;
+    private String ID;
+
+}
