142781402c187dd4019633e4dd8009e63bc769cc
jefflee0510 <jeffreylee0510@gmail.com>
2018-04-01 23:42:57 +1000

    Moved displayMenu() from Driver.java to Menu.java

--- src/Driver.java
+++ src/Driver.java
@@ -55,26 +55,4 @@ public class Driver
         login(inputUser, inputPass);
     }
     
-    public static void displayMenu(String userType) //prints menu after login(), menu differs based on userType
-    {
-    	if(userType == "CasualStaff")
-    	{
-    		System.out.println("1)\tViewassigned activities");
-    		System.out.println("2)\tSee offer and application");
-    	}
-    	else if(userType == "Approval")
-    	{
-    		System.out.println("1)\tView assignment requests");
-    	}
-    	else if(userType == "CourseCoordinator")
-    	{
-    		System.out.println("1)\tView assigned courses");
-    		System.out.println("2)\tRequest onboarding for new casual staff member then notify admin");
-    	}
-    	else if(userType == "Admin")
-    	{
-    		System.out.println("1)\tMaintain and update casual staff list");
-    		System.out.println("2)\tManage payroll");
-    	}
-    }
 }
