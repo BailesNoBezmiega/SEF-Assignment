b66aab0988c8273104f7896dae11cc6c46e70bb1
David Doan <s3686810@student.rmit.edu.au>
2018-04-04 19:58:01 +1000

    Added constructors for approval and casual staff classes

--- src/model/Approval.java
+++ src/model/Approval.java
@@ -1,8 +1,21 @@
 package model;
+import java.io.*;
 
 public class Approval extends Staff
 {
     // ** TO DO **
+    public Approval(String username)
+    {
+        this.inputFile = new File("Approval.txt");
+        readFile(username);
+        this.userName = readUser;
+        this.ID = readID;
+        this.name = readName;
+        this.phoneNumber = readPhoneNo;
+        this.emailAddress = readEmail;
+
+    }
+
     public void menu()
     {
 
--- src/model/CasualStaff.java
+++ src/model/CasualStaff.java
@@ -1,8 +1,22 @@
 package model;
 
+import java.io.*;
+
 public class CasualStaff extends Staff
 {
     // ** TO DO **
+    public CasualStaff(String username)
+    {
+        this.inputFile = new File("CasualStaff.txt");
+        readFile(username);
+        this.userName = readUser;
+        this.ID = readID;
+        this.name = readName;
+        this.phoneNumber = readPhoneNo;
+        this.emailAddress = readEmail;
+
+    }
+
     public void menu()
     {
 
